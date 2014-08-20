5e843f29bba4d9e32f21e9de486ff4df11b56baf
Ethan Otterlei <otterlei@gmail.com>
2014-08-20 13:13:16 -0500

    Shift header off input_array

--- lib/smarter_csv/smarter_csv.rb
+++ lib/smarter_csv/smarter_csv.rb
@@ -195,7 +195,8 @@ module SmarterCSV
       if options[:headers_in_file]        # extract the header line
         # process the header line in the CSV file..
         # the first line of a CSV file contains the header .. it might be commented out, so we need to read it anyhow
-        header = input.split(options[:row_sep]).first
+        input_array = input.split(options[:row_sep])
+        header = input_array.shift
         line_count += 1
         header = header.gsub(options[:strip_chars_from_headers], '') if options[:strip_chars_from_headers]
         if (header =~ %r{#{options[:quote_char]}}) and (! options[:force_simple_split])
@@ -252,7 +253,7 @@ module SmarterCSV
       # now on to processing all the rest of the lines in the CSV file:
       #while ! f.eof?    # we can't use f.readlines() here, because this would read the whole file into memory at once, and eof => true
       num_lines = input.split(options[:row_sep]).size
-      input.split(options[:row_sep]).each_with_index do |line, index|
+      input_array.each_with_index do |line, index|
         puts "LINE: #{index}    #{line}"
         #line = f.readline  # read one line.. this uses the input_record_separator $/ which we set previously!
         line_count += 1
